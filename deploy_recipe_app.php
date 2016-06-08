7361fbe9d39018b7a4fa8a5882dd241a85554308
Tanguy Giton <tanguy.giton@gmail.com>
2016-06-08 16:08:57 +0200

    Fix mail not send

--- deploy_recipe_app.php
+++ deploy_recipe_app.php
@@ -8,6 +8,8 @@ task('deploy:fos:js-routing:dump', function () {
 
 })->desc('FOS JS Routing Dump');
 
+after('deploy:vendors', 'deploy:fos:js-routing:dump');
+
 task('deploy:assetic:dump', function () {
     if (!get('dump_assets')) {
         return;
@@ -18,8 +20,6 @@ task('deploy:assetic:dump', function () {
     run('{{bin/php}} {{release_path}}/' . trim(get('bin_dir'), '/') . '/console assetic:dump --env={{env}} --no-debug');
 
 })->desc('Dump assets');
-
-after('deploy:vendors', 'deploy:fos:js-routing:dump');
 
 $shared_dir = get('shared_dirs');
 $shared_dir[] = 'web/images/profile';
